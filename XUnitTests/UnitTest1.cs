4673c86b47c49599286dd88badfb8a847607d451
Chernyshelly <andreycherni@gmail.com>
2020-06-11 10:16:02 +0300

    tests ready

--- XUnitTests/UnitTest1.cs
+++ XUnitTests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace exApp.XUnitTests
@@ -6,9 +8,41 @@ namespace exApp.XUnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void BasicWork()
         {
-            Assert.True(true);
+            List<int> a = new List<int>() { 5, 8, -15, -5, 25, -25 };
+            List<int[]> list = Program.GetZeroSumms(a);
+            if ((list[0] == new int[2] { -25, 25}) && (list[1] == new int[2] { -5, 5 }))
+            {
+                Assert.True(true);
+            }
+        }
+
+        [Fact]
+        public void NoResult()
+        {
+            List<int> a = new List<int>() { 5, 8, -15, -25 };
+            List<int[]> list = Program.GetZeroSumms(a);
+            Assert.Equal(new List<int[]>(), list);
+        }
+
+        [Fact]
+        public void EmptyInput()
+        {
+            List<int> a = new List<int>();
+            List<int[]> list = Program.GetZeroSumms(a);
+            Assert.Equal(new List<int[]>(), list);
+        }
+
+        [Fact]
+        public void MultipleEqualNumbers()
+        {
+            List<int> a = new List<int>() { 5, 8, -15, -5, 25, -25, 5, -25 };
+            List<int[]> list = Program.GetZeroSumms(a);
+            if ((list[0] == new int[2] { -25, 25 }) && (list[1] == new int[2] { -5, 5 }) && list.Count == 2)
+            {
+                Assert.True(true);
+            }
         }
     }
 }
--- exApp/Program.cs
+++ exApp/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace exApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
