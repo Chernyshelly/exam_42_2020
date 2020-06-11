3cac9abf2d5d37331e59bc6077d3797a4e216622
Chernyshelly <andreycherni@gmail.com>
2020-06-11 09:27:51 +0300

    program ready no check

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace exam_42_2020
 {
@@ -7,6 +8,32 @@ namespace exam_42_2020
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        public static List<int[]> GetZeroSumms(List<int> list)
+        {
+            List<int> vvod = list;
+            vvod.Sort();
+            List<int[]> result = new List<int[]>();
+            int i = 0;
+            int j = vvod.Count - 1;
+            while (j > i)
+            {
+                while (Math.Abs(vvod[j]) > Math.Abs(vvod[i]))
+                {
+                    j--;
+                }
+
+                if(vvod[i] == 0 - vvod[j])
+                {
+                    result.Add(new int[2] { vvod[i], vvod[j] });
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return result;
         }
     }
 }
