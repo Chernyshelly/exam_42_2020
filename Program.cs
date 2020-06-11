6849edad94a5f1f0f5b99f9102adb0e50f38d987
Chernyshelly <andreycherni@gmail.com>
2020-06-11 08:15:58 +0300

    project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exam_42_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
